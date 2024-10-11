733d1c7b92cec94931b1d34013b4bcbf651f10bf
bigparkzero <ljjok1234@gmail.com>
2024-10-11 11:54:25 +0900

    あWFだｓｄｓ

--- Assets/3. Script/UI/Compass/WorldPositionUiMarkVerticalLine.cs
+++ Assets/3. Script/UI/Compass/WorldPositionUiMarkVerticalLine.cs
@@ -29,31 +29,8 @@ public class WorldPositionUiMarkVerticalLine : MonoBehaviour
             line.SetPosition(1, hit.point);
 
             // 레이가 맞은 위치로 메쉬 이동 (버텍스 데이터 변경)
-            MoveMeshToPosition(hit.point);
         }
     }
 
-    // 메쉬의 버텍스 데이터를 레이 충돌 위치로 이동시키는 함수
-    private void MoveMeshToPosition(Vector3 targetPosition)
-    {
-        // 메쉬 데이터를 가져옴
-        Mesh mesh = meshF.mesh;
-        Vector3[] vertices = mesh.vertices;
-
-        // 버텍스 이동을 위한 오프셋 계산
-        Vector3 offset = targetPosition - transform.position;
-
-        // 각 버텍스 위치에 오프셋을 적용하여 이동
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            vertices[i] += offset;
-        }
-
-        // 변경된 버텍스 데이터를 메쉬에 다시 적용
-        mesh.vertices = vertices;
-
-        // 메쉬를 업데이트하여 변형된 것이 반영되도록 함
-        mesh.RecalculateBounds();
-        mesh.RecalculateNormals();
-    }
+   
 }
